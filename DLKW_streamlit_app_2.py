003b676fdf6dd9cda4a36e77d06593632f67f851
Ruthran <ruthrangee@gmail.com>
2023-10-23 12:08:10 +0530

    Update DLKW_streamlit_app_2.py
    intent

--- DLKW_streamlit_app_2.py
+++ DLKW_streamlit_app_2.py
@@ -4,6 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 try:
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
+  streamlit.title('Zenas Amazing Athleisure Catalog')
 except URLError as e:
   Streamlit.error()
