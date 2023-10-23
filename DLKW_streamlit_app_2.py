24c43b8fb7d4b34709c8d406abc001fe25132287
Ruthran <ruthrangee@gmail.com>
2023-10-23 12:06:39 +0530

    Update DLKW_streamlit_app_2.py
    URL Error

--- DLKW_streamlit_app_2.py
+++ DLKW_streamlit_app_2.py
@@ -1,5 +1,9 @@
-
 import streamlit
-import snowflake.connector
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+try
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
+except URLError as e:
+  Streamlit.error()
